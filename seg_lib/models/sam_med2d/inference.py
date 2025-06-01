29bd0d1461f5b7ed1f36e7710369a89cc4e054c8
Zanoni <gzf1996@gmail.com>
2025-06-01 14:37:27 +0200

    fix: sam-med2d inference class

--- seg_lib/models/sam_med2d/inference.py
+++ seg_lib/models/sam_med2d/inference.py
@@ -1,4 +1,4 @@
-import torch
+import numpy as np
 
 from seg_lib.models.selector import predictor_selector
 from seg_lib.models.sam.inference import SamInference
@@ -13,42 +13,42 @@ class SamMed2dInference(SamInference):
         self.th = self.predictor.mask_threshold
         self.device = device
 
-    def segment_bbox(self, boxes: list = None):
-        if boxes is None or len(boxes) == 0:
+    def predict(self, sam_inputs: list):
+        if sam_inputs is None or len(sam_inputs) == 0:
             return None
-        
-        boxes = torch.tensor(boxes, device=self.predictor.device)
-        boxes = self.predictor.apply_boxes_torch(
-            boxes, self.predictor.original_size, self.predictor.new_size
-        )
-        masks, scores, _ = self.predictor.predict_torch(
-            point_coords=None,
-            point_labels=None,
-            boxes=boxes,
-            multimask_output=True,
-            return_logits=False
-        )
-        out_size = (len(boxes), *self.predictor.original_size)
-        return self.unify_mask(out_size, masks, scores)
+
+        masks_local = []
+        scores_local = []
+        for inp in sam_inputs:
+            masks, scores, _ = self.predictor.predict(
+                **inp,
+                multimask_output=True,
+                return_logits=False
+            )
+            masks_local.append(masks)
+            scores_local.append(scores)
+
+        out_size = (len(sam_inputs), *self.predictor.original_size)
+        return self.unify_mask(out_size, np.array(masks_local), np.array(scores_local))
+
+    def segment_bbox(self, boxes: list = None):
+        sam_input = [
+            {
+                "point_coords": None,
+                "point_labels": None,
+                "box": bbox
+            }
+            for bbox in boxes
+        ]
+        return self.predict(sam_input)
 
     def segment_point(self, points: list = None):
-        if points is None or len(points) == 0:
-            return None
-
-        points = torch.tensor(points, device=self.predictor.device)
-        points = points[:, None, ...] # adds an additional axis
-        points = self.predictor.apply_coords_torch(
-            points, self.predictor.original_size, self.predictor.new_size
-        )
-        labels = [[1] for _ in range(len(points))]
-        labels = torch.tensor(labels, device=self.predictor.device)
-
-        masks, scores, _ = self.predictor.predict_torch(
-            point_coords=points,
-            point_labels=labels,
-            boxes=None,
-            multimask_output=True,
-            return_logits=False
-        )
-        out_size = (len(points), *self.predictor.original_size)
-        return self.unify_mask(out_size, masks, scores)
+        sam_input = [
+            {
+                "point_coords": np.array(point[None, ...]),
+                "point_labels": np.ones((1,), dtype=np.int64),
+                "box": None
+            }
+            for point in points
+        ]
+        return self.predict(sam_input)
